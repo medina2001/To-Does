c0c7e69d0062340be8e33e994c08f2078cbc3db6
Gabriel Maciel <gabrielmaciel2310@gmail.com>
2021-06-03 00:20:49 -0400

    Initial Commit

---
+++ TooDoes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TooDoes
+//
+//  Created by Gabriel de Oliveira Maciel on 03/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TooDoes/TooDoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TooDoesApp.swift
+//  TooDoes
+//
+//  Created by Gabriel de Oliveira Maciel on 03/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TooDoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
